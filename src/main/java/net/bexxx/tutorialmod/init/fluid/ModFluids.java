5ea16411375c02e8b254a6e82ce4f722bbde410a
DancerVlt69 <dancervlt69@googlemail.com>
2023-02-02 09:26:35 +0100

    simplified: using MODID

--- src/main/java/net/bexxx/tutorialmod/init/fluid/ModFluids.java
+++ src/main/java/net/bexxx/tutorialmod/init/fluid/ModFluids.java
@@ -1,11 +1,8 @@
-package net.bexxx.tutorialmod.fluid;
+package net.bexxx.tutorialmod.init.fluid;
 
-import net.bexxx.tutorialmod.tab.ModItemGroup;
+import net.bexxx.tutorialmod.init.util.tab.ModItemGroup;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.bexxx.tutorialmod.TutorialMod;
-
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.FluidBlock;
@@ -13,9 +10,10 @@ import net.minecraft.fluid.FlowableFluid;
 import net.minecraft.item.BucketItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
-
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+
+import static net.bexxx.tutorialmod.TutorialMod.MODID;
 
 public class ModFluids {
     public static FlowableFluid STILL_SOAP_WATER;
@@ -25,14 +23,15 @@ public class ModFluids {
 
     public static void register() {
         STILL_SOAP_WATER = Registry.register(Registry.FLUID,
-                new Identifier(TutorialMod.MODID, "soap_water"), new SoapWaterFluid.Still());
+                new Identifier(MODID, "soap_water"), new SoapWaterFluid.Still());
         FLOWING_SOAP_WATER = Registry.register(Registry.FLUID,
-                new Identifier(TutorialMod.MODID, "flowing_soap_water"), new SoapWaterFluid.Flowing());
+                new Identifier(MODID, "flowing_soap_water"), new SoapWaterFluid.Flowing());
 
-        SOAP_WATER_BLOCK = Registry.register(Registry.BLOCK, new Identifier(TutorialMod.MODID, "soap_water_block"),
+        SOAP_WATER_BLOCK = Registry.register(Registry.BLOCK, new Identifier(MODID, "soap_water_block"),
                 new FluidBlock(ModFluids.STILL_SOAP_WATER, FabricBlockSettings.copyOf(Blocks.WATER)){ });
-        SOAP_WATER_BUCKET = Registry.register(Registry.ITEM, new Identifier(TutorialMod.MODID, "soap_water_bucket"),
-                new BucketItem(ModFluids.STILL_SOAP_WATER, new FabricItemSettings().recipeRemainder(Items.BUCKET).maxCount(1).group(ModItemGroup.PLANTS)));
+        SOAP_WATER_BUCKET = Registry.register(Registry.ITEM, new Identifier(MODID, "soap_water_bucket"),
+                new BucketItem(ModFluids.STILL_SOAP_WATER, new FabricItemSettings().recipeRemainder(Items.BUCKET)
+                                .maxCount(1).group(ModItemGroup.PLANTS)));
 
 
     }}
