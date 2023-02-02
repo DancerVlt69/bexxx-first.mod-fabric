740e277f269fc89e11f67493ddfe7d2e84f77901
DancerVlt69 <dancervlt69@googlemail.com>
2023-02-02 06:39:39 +0100

    changed: name of Tab4 to "OMEGA_TOOLS"

--- src/main/java/net/bexxx/tutorialmod/init/util/tab/ModItemGroup.java
+++ src/main/java/net/bexxx/tutorialmod/init/util/tab/ModItemGroup.java
@@ -1,6 +1,6 @@
-package net.bexxx.tutorialmod.tab;
+package net.bexxx.tutorialmod.init.util.tab;
 
-import net.bexxx.tutorialmod.item.ModItems;
+import net.bexxx.tutorialmod.init.items.ModItems;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
@@ -16,7 +16,7 @@ public class ModItemGroup {
 			new Identifier(MODID, "tab2"), () -> new ItemStack(ModItems.EIGHT_BALL));
 	public static final ItemGroup TANZANITE_TOOLS = FabricItemGroupBuilder.build(
 			new Identifier(MODID, "tab3"), () -> new ItemStack(ModItems.TANZANITE_SHARD));
-	public static final ItemGroup OMEGA = FabricItemGroupBuilder.build(
+	public static final ItemGroup OMEGA_TOOLS = FabricItemGroupBuilder.build(
 			new Identifier(MODID, "tab4"), () -> new ItemStack(ModItems.OMEGA_PICKAXE));
 	public static final ItemGroup PLANTS = FabricItemGroupBuilder.build(
 			new Identifier(MODID, "tab5"), () -> new ItemStack(ModItems.EGGPLANT));
