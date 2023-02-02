2e186fbb3ad2e8c06f88b7c93735b9fdb59aa7d4
DancerVlt69 <dancervlt69@googlemail.com>
2023-02-02 08:59:30 +0100

    simplified: Tooltip usage

--- src/main/java/net/bexxx/tutorialmod/init/items/custom/OmegaPickaxeItem.java
+++ src/main/java/net/bexxx/tutorialmod/init/items/custom/OmegaPickaxeItem.java
@@ -1,15 +1,26 @@
 package net.bexxx.tutorialmod.init.items.custom;
 
 
+import net.bexxx.tutorialmod.init.util.tolltips.ShowToolTips;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.PickaxeItem;
 import net.minecraft.item.ToolMaterial;
+import net.minecraft.text.Text;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class OmegaPickaxeItem extends PickaxeItem {
+	private final String tooltipShift;
 	private final int enchantability;
-	public OmegaPickaxeItem(ToolMaterial material, int attackDamage, float attackSpeed, int enchantability, Settings settings) {
+	public OmegaPickaxeItem(ToolMaterial material, int attackDamage, float attackSpeed, int enchantability,
+							Settings settings, String tooltipShift) {
 		super(material, attackDamage, attackSpeed, settings);
 
 		this.enchantability = enchantability;
+		this.tooltipShift = tooltipShift;
 	}
 
 	@Override
@@ -17,5 +28,9 @@ public class OmegaPickaxeItem extends PickaxeItem {
 		return enchantability; // return 50;
 	}
 
-
+	@Override
+	public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+		ShowToolTips.showTooltips(tooltip, tooltipShift);
+		// super.appendTooltip(stack, world, tooltip, context);
+	}
 }
